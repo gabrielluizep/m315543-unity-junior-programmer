3ba4cdf5d92e8409035db8496e1e7e9b13f5856d
Gabriel Espindola <gabrielluizep.glep@gmail.com>
2023-03-22 15:03:30 +0000

    :sparkles: added local multiplayer

---
+++ Assets/Course Library/Scripts/SecondFollowPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondFollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset = new Vector3(0, 5, -7);
+    public Vector3 firstPersonOffset = new Vector3(0, 3, -0.5f);
+    public bool firstPerson = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            firstPerson = !firstPerson;
+        }
+
+        transform.position = player.transform.position + (firstPerson ? firstPersonOffset : offset);
+    }
+}
