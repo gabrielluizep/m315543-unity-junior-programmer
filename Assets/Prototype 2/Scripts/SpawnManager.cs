f6a3217a034caa4cf1815f8dc2d8e51e7aaaa9bd
Gabriel Espindola <gabrielluizep.glep@gmail.com>
2023-03-24 12:37:42 +0000

    :sparkles: added collisions

--- Assets/Prototype 2/Scripts/SpawnManager.cs
+++ Assets/Prototype 2/Scripts/SpawnManager.cs
@@ -8,20 +8,26 @@ public class SpawnManager : MonoBehaviour
     public float spawnRangeX = 20;
     public float spawnPosZ = 20;
 
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S)){
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalsPrefabs.Length);
+      
+    }
 
-            Instantiate(animalsPrefabs[animalIndex], spawnPos, animalsPrefabs[animalIndex].transform.rotation);    
-        }
+    void SpawnRandomAnimal () {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalsPrefabs.Length);
+
+        Instantiate(animalsPrefabs[animalIndex], spawnPos, animalsPrefabs[animalIndex].transform.rotation);
     }
 }
