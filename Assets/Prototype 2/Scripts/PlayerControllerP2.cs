7892b6d9b2fa8c924729af87fb9e38acbb64a714
Gabriel Espindola <gabrielluizep.glep@gmail.com>
2023-03-31 12:54:38 +0100

    :construction: bonus features prototype 2

--- Assets/Prototype 2/Scripts/PlayerControllerP2.cs
+++ Assets/Prototype 2/Scripts/PlayerControllerP2.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerControllerP2 : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
-    public float xRange = 10;
+    public float xMax = 10;
+    public float xMin = -10;
+    public float zMax = 10;
+    public float zMin = 0;
 
     public GameObject projectilePrefab;
 
@@ -19,14 +23,24 @@ public class PlayerControllerP2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < -xRange)
+        if(transform.position.x < xMin)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xMin, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.x > xRange)
+        if (transform.position.x > xMax)
         {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xMax, transform.position.y, transform.position.z);
+        }
+
+         if(transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
         }
 
         if(Input.GetKeyDown(KeyCode.Space)){
@@ -35,5 +49,8 @@ public class PlayerControllerP2 : MonoBehaviour
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
     }
 }
