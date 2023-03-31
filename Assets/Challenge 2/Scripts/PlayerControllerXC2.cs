935d86f7ca9b5fa07f0bd89a9de4b619aa3efce7
Gabriel Espindola <gabrielluizep.glep@gmail.com>
2023-03-31 11:53:46 +0100

    :sparkles: finished challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerXC2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerXC2 : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooldown = 1.0f;
+    private float lastShot = 0.0f;
+
+    void Start()
+    {
+        lastShot = Time.time;   
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > lastShot + cooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            lastShot = Time.time;
+        }
+    }
+}
